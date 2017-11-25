edb859ff144926f655f91b1a8a37e206a1718f0d
Akshay Channabasaiah <aksc.champ@gmail.com>
2017-11-25 07:45:01 +0200

    Base template ready

--- fullstack_template/static/webpack.config.js
+++ fullstack_template/static/webpack.config.js
@@ -1,6 +1,17 @@
 const webpack = require('webpack');
 
 const config = {
+    module: {
+        rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader?cacheDirectory=true',
+        }        
+        }
+      ]
+    },
     entry:  __dirname + '/js/index.jsx',
     output: {
         path: __dirname + '/dist',
